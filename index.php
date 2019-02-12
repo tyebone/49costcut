d70d977effd0cb69a16fee0b56a5251c184cee45
内田佳世 <pauole.noah.hoku@gmail.com>
2019-02-12 17:34:25 +0800

    TOP画面作成
    ヘッダーの作成(文字サイズは要相談)
    TOP背景画表示(画像テンプレートのまま)

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
@@ -19,10 +18,48 @@
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="./assets/demo/demo.css" rel="stylesheet" />
 </head>
-<body>
+<body class="index-page sidebar-collapse">
 
 
-
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="">
+          TOP </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-togglez nav-link" >About</a>
+           <!--  <div class="dropdown-menu dropdown-with-icons">
+            </div> -->
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">管理者用ログイン</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('./assets/img/bg2.jpg');">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand">
+            <h1>Material Kit.</h1>
+            <h3>A Badass Bootstrap 4 UI Kit based on Material Design.</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="main main-raised">
+  </div>
 
 <!-- JSとかの実装 -->
   <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
