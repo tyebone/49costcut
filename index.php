37355d82e59c93c6a5d72b86461a92102accc8c5
内田佳世 <pauole.noah.hoku@gmail.com>
2019-02-13 16:31:18 +0800

    index.phpのデザイン変更
    cssを排除し、htmlでテキスト表示のみ実装
    ボタン部分のみ記述
    デザインは１から作り直す

--- index.php
+++ index.php
@@ -3,206 +3,122 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     セブ生活費シュミレーター
   </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="./assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="./assets/demo/demo.css" rel="stylesheet" />
 </head>
 
-<body class="index-page sidebar-collapse">
-  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
-    <div class="container">
-      <div class="navbar-translate">
-        <a class="navbar-brand" href="">
-          TOP </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="">About</a>
+<body>
+  <div>
+    <a class="" href="">TOP </a>
+    <a class="" href="">About</a>
+    <a class="" href="">管理者ログイン</a>
+  </div>
+  <div>
+
+
+
+
+
+
+  </div>
+  <div>
+    <h2>Q1.何とかかんとか</h2>
+      <select name="sample1">
+        <option value="1">選択肢１</option>
+        <option value="2">選択肢２</option>
+        <option value="3">選択肢３</option>
+        <option value="4">選択肢４</option>
+        <option value="5">選択肢５</option>
+      </select>
+  </div>
+  <div>
+    <h2>Q2.何とかかんとか</h2>
+      <select name="sample1">
+        <option value="1">選択肢１</option>
+        <option value="2">選択肢２</option>
+        <option value="3">選択肢３</option>
+        <option value="4">選択肢４</option>
+        <option value="5">選択肢５</option>
+      </select>
+    </div>
+    <div>
+      <h2>Q3.何とかかんとか</h2>
+        <select name="sample1">
+          <option value="1">選択肢１</option>
+          <option value="2">選択肢２</option>
+          <option value="3">選択肢３</option>
+          <option value="4">選択肢４</option>
+          <option value="5">選択肢５</option>
+        </select>
+    </div>
+    <div>
+      <h2>Q4.何とかかんとか</h2>
+        <select name="sample1">
+          <option value="1">選択肢１</option>
+          <option value="2">選択肢２</option>
+          <option value="3">選択肢３</option>
+          <option value="4">選択肢４</option>
+          <option value="5">選択肢５</option>
+        </select>
+    </div>
+    <div>
+      <h2>Q5.何とかかんとか</h2>
+        <select name="sample1">
+          <option value="1">選択肢１</option>
+          <option value="2">選択肢２</option>
+          <option value="3">選択肢３</option>
+          <option value="4">選択肢４</option>
+          <option value="5">選択肢５</option>
+        </select>
+    </div>
+    <div>
+      <h2>Q6.何とかかんとか</h2>
+        <select name="sample1">
+          <option value="1">選択肢１</option>
+          <option value="2">選択肢２</option>
+          <option value="3">選択肢３</option>
+          <option value="4">選択肢４</option>
+          <option value="5">選択肢５</option>
+        </select>
+    </div>
+          <!-- checkbox -->
+    <div>
+      <h2>Q.選択してください(複数選択可)</h2>
+        <ul style="list-style: none;">
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 選択肢
+            </label>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="">管理者ログイン</a>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 選択肢
+            </label>
+          </li>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 選択肢
+            </label>
+          </li>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 選択肢
+            </label>
+          </li>
+          <li>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 選択肢
+            </label>
+          </li>
           </li>
         </ul>
-      </div>
+        <div class="">
+          <a href="" class="" target="">
+            結果表示
+          </a>
+        </div>
     </div>
-  </nav>
-  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('./assets/img/bg2.jpg');">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 ml-auto mr-auto">
-          <div class="brand">
-            <h1>セブカツ</h1>
-            <h3>サブタイトル</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="main main-raised">
-    <div class="section section-basic">
-      <div class="container">
-        <div class="space-70"></div>
-
-        <!-- checkbox -->
-              <div class="form-check text-center">
-                <ul style="list-style: none;">
-                  <li>
-                    <label class="form-check-label">
-                      <input class="form-check-input" type="checkbox" value=""> Unchecked
-                      <span class="form-check-sign">
-                        <span class="check"></span>
-                      </span>
-                    </label>
-                  </li>
-                  <!-- <div class="space-70"></div> -->
-                  <li>
-                    <label class="form-check-label">
-                      <input class="form-check-input" type="checkbox" value=""> Unchecked
-                      <span class="form-check-sign">
-                        <span class="check"></span>
-                      </span>
-                    </label>
-                  </li>
-                </ul>
-            </div>
-        </div>
-        <div class="space-70"></div>
-        <!-- 結果表示ボタン (class="btn btn-roseで色を変更可)-->
-          <div class="col-sm-8 col-md-5 ml-auto mr-auto text-center">
-            <a href="" class="btn btn-rose btn-upgrade btn-lg" target="_blank">
-              結果表示
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Classic Modal -->
-  <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <i class="material-icons">clear</i>
-          </button>
-        </div>
-
-        <div class="modal-footer">
-          <button type="button" class="btn btn-link">Nice Button</button>
-          <button type="button" class="btn btn-danger btn-link" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--  End Modal -->
-  <footer class="footer" data-background-color="black">
-
-  </footer>
-  <!--   Core JS Files   -->
-  <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="./assets/js/plugins/moment.min.js"></script>
-  <!--  Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="./assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="./assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
-  <script>
-    $(document).ready(function() {
-      //init DateTimePickers
-      materialKit.initFormExtendedDatetimepickers();
-
-      // Sliders Init
-      materialKit.initSliders();
-    });
-
-
-    function scrollToDownload() {
-      if ($('.section-download').length != 0) {
-        $("html, body").animate({
-          scrollTop: $('.section-download').offset().top
-        }, 1000);
-      }
-    }
-
-
-    $(document).ready(function() {
-
-      $('#facebook').sharrre({
-        share: {
-          facebook: true
-        },
-        enableHover: false,
-        enableTracking: false,
-        enableCounter: false,
-        click: function(api, options) {
-          api.simulateClick();
-          api.openPopup('facebook');
-        },
-        template: '<i class="fab fa-facebook-f"></i> Facebook',
-        url: 'https://demos.creative-tim.com/material-kit/index.html'
-      });
-
-      $('#googlePlus').sharrre({
-        share: {
-          googlePlus: true
-        },
-        enableCounter: false,
-        enableHover: false,
-        enableTracking: true,
-        click: function(api, options) {
-          api.simulateClick();
-          api.openPopup('googlePlus');
-        },
-        template: '<i class="fab fa-google-plus"></i> Google',
-        url: 'https://demos.creative-tim.com/material-kit/index.html'
-      });
-
-      $('#twitter').sharrre({
-        share: {
-          twitter: true
-        },
-        enableHover: false,
-        enableTracking: false,
-        enableCounter: false,
-        buttons: {
-          twitter: {
-            via: 'CreativeTim'
-          }
-        },
-        click: function(api, options) {
-          api.simulateClick();
-          api.openPopup('twitter');
-        },
-        template: '<i class="fab fa-twitter"></i> Twitter',
-        url: 'https://demos.creative-tim.com/material-kit/index.html'
-      });
-
-    });
-  </script>
 </body>
-
 </html>
