be2f153daab0cd11697c0664518d082e76df483d
内田佳世 <pauole.noah.hoku@gmail.com>
2019-02-13 18:35:34 +0800

    index.phpのhtml部分作成
    細かいデザインや画像などは未定
    画像、ロゴ、タイトル、サイト説明文は未入力
    大まかな枠組とquestionみの完成

--- index.php
+++ index.php
@@ -15,103 +15,116 @@
     <a class="" href="">管理者ログイン</a>
   </div>
   <div>
-
-
-
-
-
-
+    <h1>タイトルor画像やロゴ</h1>
   </div>
   <div>
-    <h2>Q1.何とかかんとか</h2>
+    <h2>サイト説明文</h2>
+    <p>aaaaaaaaaaaaaaaaa</p>
+  </div>
+
+  <div>
+    <h2>Q1.年齢と性別を選んでください</h2>
       <select name="sample1">
-        <option value="1">選択肢１</option>
-        <option value="2">選択肢２</option>
-        <option value="3">選択肢３</option>
-        <option value="4">選択肢４</option>
-        <option value="5">選択肢５</option>
+        <option value="1">29歳以下の男性</option>
+        <option value="2">29歳以下の女性</option>
+        <option value="3">30歳以上の男性</option>
+        <option value="4">30歳以上の女性</option>
       </select>
   </div>
   <div>
-    <h2>Q2.何とかかんとか</h2>
+    <h2>Q2.朝食はどれぐらい食べますか</h2>
       <select name="sample1">
-        <option value="1">選択肢１</option>
-        <option value="2">選択肢２</option>
-        <option value="3">選択肢３</option>
-        <option value="4">選択肢４</option>
-        <option value="5">選択肢５</option>
+        <option value="1">食べない or 学校で用意されている</option>
+        <option value="2">かる〜く食べる</option>
+        <option value="3">しっかり食べる</option>
       </select>
     </div>
     <div>
-      <h2>Q3.何とかかんとか</h2>
+      <h2>Q3.昼食はどれぐらい食べますか</h2>
         <select name="sample1">
-          <option value="1">選択肢１</option>
-          <option value="2">選択肢２</option>
-          <option value="3">選択肢３</option>
-          <option value="4">選択肢４</option>
-          <option value="5">選択肢５</option>
+          <option value="1">食べないor学校で用意されている</option>
+          <option value="2">かる〜く食べる</option>
+          <option value="3">しっかり食べる</option>
         </select>
     </div>
     <div>
-      <h2>Q4.何とかかんとか</h2>
+      <h2>Q4.夕食はどれぐらい食べますか</h2>
         <select name="sample1">
-          <option value="1">選択肢１</option>
-          <option value="2">選択肢２</option>
-          <option value="3">選択肢３</option>
-          <option value="4">選択肢４</option>
-          <option value="5">選択肢５</option>
+          <option value="1">食べないor学校で用意されている</option>
+          <option value="2">かる〜く食べる</option>
+          <option value="3">しっかり食べる</option>
         </select>
     </div>
     <div>
-      <h2>Q5.何とかかんとか</h2>
+      <h2>Q5.主な交通手段はなんですか</h2>
         <select name="sample1">
-          <option value="1">選択肢１</option>
-          <option value="2">選択肢２</option>
-          <option value="3">選択肢３</option>
-          <option value="4">選択肢４</option>
-          <option value="5">選択肢５</option>
+          <option value="1">徒歩</option>
+          <option value="2">ジプニー</option>
+          <option value="3">バイクタクシー</option>
+          <option value="4">グラブor タクシー</option>
         </select>
     </div>
     <div>
-      <h2>Q6.何とかかんとか</h2>
+      <h2>Q6.洗濯の頻度はどれくらいですか</h2>
         <select name="sample1">
-          <option value="1">選択肢１</option>
-          <option value="2">選択肢２</option>
-          <option value="3">選択肢３</option>
-          <option value="4">選択肢４</option>
-          <option value="5">選択肢５</option>
+          <option value="1">自分で手洗い</option>
+          <option value="2">週1〜2回</option>
+          <option value="3">週3回以上</option>
         </select>
     </div>
-          <!-- checkbox -->
     <div>
-      <h2>Q.選択してください(複数選択可)</h2>
+      <h2>Q7.あなたの土日の過ごし方は</h2>
+        <select name="sample1">
+          <option value="1">どこにも行かない</option>
+          <option value="2">日帰り旅行に行きたい</option>
+          <option value="3">泊まりで旅行に行きたい</option>
+        </select>
+    </div>
+    <div>
+      <h2>Q8.マッサージは行きたいですか</h2>
+        <select name="sample1">
+          <option value="1">特に行きたくない</option>
+          <option value="2">リーズナブルなところに行きたい</option>
+          <option value="3">週高級スパに行きたい</option>
+        </select>
+    </div>
+    <div>
+      <h2>Q9.当てはまるものを選択してください(複数選択可)</h2>
         <ul style="list-style: none;">
           <li>
             <label class="">
-              <input class="" type="checkbox" value=""> 選択肢
+              <input class="" type="checkbox" value=""> おみやげを買いたい
             </label>
           </li>
           <li>
             <label class="">
-              <input class="" type="checkbox" value=""> 選択肢
+              <input class="" type="checkbox" value=""> 現地の服が欲しい
             </label>
           </li>
           <li>
             <label class="">
-              <input class="" type="checkbox" value=""> 選択肢
+              <input class="" type="checkbox" value=""> 習い事を始めたい
             </label>
           </li>
           <li>
             <label class="">
-              <input class="" type="checkbox" value=""> 選択肢
+              <input class="" type="checkbox" value=""> お酒を飲みたい
             </label>
           </li>
-          <li>
           <li>
             <label class="">
-              <input class="" type="checkbox" value=""> 選択肢
+              <input class="" type="checkbox" value=""> タバコを吸いたい
             </label>
           </li>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> カジノに行きたい
+            </label>
+          </li>
+          <li>
+            <label class="">
+              <input class="" type="checkbox" value=""> 夜のスポットに遊びに行きたい
+            </label>
           </li>
         </ul>
         <div class="">
@@ -120,5 +133,8 @@
           </a>
         </div>
     </div>
+    <footer>
+
+    </footer>
 </body>
 </html>
