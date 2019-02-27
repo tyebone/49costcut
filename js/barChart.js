66fdafff4b33380a618431b843b8c8873554747b
今竹太樹 <imamura201611@gmail.com>
2019-02-27 17:53:54 +0800

    index.htmlは<script>追加　bar.chartはラインを一本削除　4タイプのjsを追加
    index.htmlのscript追加は4タイプのファイル.jsを追加するために必要だったため追加した。しかし、状況に応じて出力をすることは実装されていない。
    bar.chartは全体のランクの平均で描画するのに1本で良いためこの削除した。

--- js/barChart.js
+++ js/barChart.js
@@ -5,17 +5,11 @@ new Chart(bCtx, {
       labels: ['貧乏神','ひもじい', '浪費家', 'ノーマル', 'ちょっとしたすごいやつ', '毎月貯金', '金持ち金持ちコース'],
       datasets: [{
 	 	label: '最高気温(度）',
-	    data: [35, 34, 37, 35, 34, 35, 34, 25],
+	    data: [35, 34, 37, 35, 34, 35, 34],
 	    borderColor: "rgba(255,0,0,1)",
 	    backgroundColor: "rgba(0,0,0,0,0,0)"
 	    },
-
-	    {
-	    label: '最低気温(度）',
-	    data: [25, 27, 27, 25, 26, 27, 25, 21],
-	    borderColor: "rgba(0,0,255,1)",
-	    backgroundColor: "rgba(0,0,0,0,0,0)"
-	    }],
+	  ],
 },
 	options: {
 		scales: {
---
+++ js/male_30.js
@@ -0,0 +1,28 @@
+//円グラフ
+const aCtx = $('#male_30')
+new Chart(aCtx, {
+    type:'pie',
+    data: {
+
+
+        labels: ['食事','交通費','雑費','旅費'],
+        datasets: [{
+            data:[40,15,20,60],
+            //値 labelsに指定した順番に紐ずく
+            //グラフの色
+            backgroundColor: [
+
+            'rgba(100, 100, 100, 0.1)',
+            'rgba(50, 50, 50, 0.2)',
+            'rgba(25, 25, 25, 0.2)',
+            'rgba(12, 12, 12, 0.2)',
+            ],
+        }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '出費割合'
+       }
+    }
+})
