699ba6fe92dc0f0a7913b97bb96b4ce4c8e50d33
yasuyukitakeshita <thetake1206@gmail.com>
2019-02-28 10:31:35 +0800

    memo_3.md追加

--- index_test.php
+++ index_test.php
@@ -8,7 +8,7 @@
 <body>
   <div class="container">
     <div class="row">
-      <div class="col-12 bg bg-danger">
+      <div class="col-12 bg bg-danger text-light">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum odit, optio aperiam beatae perspiciatis delectus ipsum neque quidem quasi dolore quas obcaecati doloremque soluta quisquam. Cupiditate  eaque quam aliquid esse?
       </div>
     </div>
@@ -28,11 +28,15 @@
       <div class="col-3 border border-success">a</div>
     </div>
   </div>
+
+
   <div class="container">
     <main class="row">
       <div class="col-sm-3 bg-danger">
         a
       </div>
+
+
       <div class="col-9">
         <div class="row">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, illo non laudantium, doloribus inventore repellendus repellat dolores voluptatem veniam quisquam quasi mollitia quos earum placeat dolor nam iusto dolorum fuga!
@@ -47,6 +51,8 @@
           <div class="col-2">a</div>
         </div>
       </div>
+
+
     </main>
   </div>
 </body>
