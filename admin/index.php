acacadb8dbc12b791224c17df046ac3592bc104d
yasuyukitakeshita <thetake1206@gmail.com>
2019-03-04 18:35:58 +0800

    admin/index.php。当日の人数の出力が完了しました。たぶん。

--- admin/index.php
+++ admin/index.php
@@ -6,32 +6,52 @@ $sql = 'SELECT `code`, `type` FROM `answers` GROUP BY `code`';
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
+// 1.通算の人数を取得
+// usersという配列を定義
 $users = [];
 while(true) {
     // レコード一件ずつ取得
     $record = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($record == false) { // レコードこれ以上ない
+    // レコードは普通、一件ずつ取得するごとに下の行に移動
+    // 最新のレコードにまで達したときに、もうこれ以上レコードを取得する必要ないよ
+    // と伝えるために、$record == false を定義する
+    if ($record == false) {
         break;
     }
+    // trueの場合、users配列に一件ずつのレコードが入っていく
     $users[] = $record;
 }
 
-// 通算
-
-    echo '<pre>';
-    var_dump(count($users));
-    echo '</pre>';
-
 // 今日だけ
 // $usersから今日のユーザーだけ抽出
+// $today変数に今日の日付を定義
+$today = date("Y/m/d");
+echo $today.'hoge'.'<br>';
+
+// $usersの一件ずつに要件定義
 foreach($users as $user) {
+    // $timeに10桁のtimeを
     $time = substr($user['code'],0,10);
-    echo $time . '<br>';
-    echo date("Y/m/d", $time) . '<br>';
+    // $user_dateは$timeで取得した10桁からそれぞれの日付を取得
+    $user_date = date("Y/m/d", $time);
 }
+
+// もしも$today(今日の日付)と$user_dataに代入されている日付が同じ場合
+// $today_numberに$usersに入っているデータの数を代入
+if($today == $user_date){
+     $today_num = count($users);
+     }else{
+    }
+
+    // echo '<pre>';
+    // var_dump($users);
+    // echo '</pre>';
+
 
 // type別
 // $usersから
+// typeがそれぞれこの数字だったら、このカテゴリーに代入
+
 
 
 
@@ -44,36 +64,31 @@ foreach($users as $user) {
 //$user_numberはユーザーの識別コード（time関数+rand関数）
 //$user_number_10は$user_numberを前半の10桁だけ取り出したもの
 //$user_number_10sは$user_number_10が連想配列になったもの
-$users_number_10s = [];
-while(true){
-    $users_number = $stmt->fetch(PDO::FETCH_ASSOC);
-    $users_number_10 = substr($users_number['code'],0,+10);
-    if($users_number_10 == false){
-        break;
-  
-
-    $users_number_10s[] = $users_number_10;
+// $users_number_10s = [];
+// while(true){
+//     $users_number = $stmt->fetch(PDO::FETCH_ASSOC);
+//     $users_number_10 = substr($users_number['code'],0,+10);
+//     if($users_number_10 == false){
+//         break;
+//     $users_number_10s[] = $users_number_10;
 
 
-    // echo '<pre>';
-    // var_dump($users_number_10s);
-    // echo '</pre>';
+//     // echo '<pre>';
+//     // var_dump($users_number_10s);
+//     // echo '</pre>';
 
-$users_number_dates= [];
-while(true){
-$users_number_date = date("Y/m/d", strtotime($users_number_10));
-if($users_number_date == false){
-    break;
-      }
-}
+// $users_number_dates= [];
+// while(true){
+// $users_number_date = date("Y/m/d", strtotime($users_number_10));
+// if($users_number_date == false){
+//     break;
+//       }
+// }
 
-$users_number_dates[] = $users_number_date;
+// $users_number_dates[] = $users_number_date;
 
-    // echo '<pre>';
-    // var_dump($users_number_dates);
-    // echo '</pre>';
- }
-}
+//  }
+// }
 
     // echo '<pre>';
     // var_dump($users_number_10s);
@@ -145,7 +160,7 @@ $users_number_dates[] = $users_number_date;
         <div class="row">
             <div class="col-xs-8 col-xs-offset-2 thumbnail">
                 <h2 class="text-center content_header">管理者画面</h2>
-                <p>本日の利用者 10人</p>
+                <p>本日の利用者 <?php echo $today_number; ?>人</p>
                 <p>通算の利用者 <?php echo count($users) ?>人</p>
                 <p>29歳以下男性の通算利用者 30人</p>
                 <p>29歳以下女性の通算利用者 30人</p>
