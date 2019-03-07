80c4c53b6f32a8cc6c10867187be43f57f85d573
内田佳世 <pauole.noah.hoku@gmail.com>
2019-03-07 10:40:48 +0800

    ログイン画面からの遷移のバグ修正

--- admin/login.php
+++ admin/login.php
@@ -39,7 +39,7 @@ if(!empty($_POST)){
             $_SESSION['49_CostCut']['id'] = $record['id'];
 
             //4.タイムライン画面に遷移
-            header('Location: index.php');
+            header('Location: count.php');
             exit();
         }else{
             //認証失敗
@@ -53,45 +53,36 @@ if(!empty($_POST)){
 
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>CebuCostsSimulator</title>
-    <link rel="stylesheet" type="text/css" href="js/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
-    <link href="js/material-kit.css?v=2.0.5" rel="stylesheet" />
-</head>
-    <body style="margin-top: 60px">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-8 col-xs-offset-2 thumbnail">
-                    <h2 class="text-center content_header">サインイン</h2>
-                    <form method="POST" action="count.php" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="email">メールアドレス</label>
-                            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
-                            <?php if(isset($errors['signin']) && $errors['signin'] =='blank'): ?>
-                                <p class= 'text-danger'>メールアドレスとパスワードを正しく入力してください</p>
-                            <?php endif; ?>
-                            <?php if(isset($errors['signin']) && $errors['signin'] =='failed'): ?>
-                                <p class= 'text-danger'>サインインに失敗しました</p>
-                            <?php endif; ?>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">パスワード</label>
-                            <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
-                        </div>
-                        <input type="submit" class="btn btn-info" value="サインイン">
-                        <span style="float: right; padding-top: 6px;">
-                            <a href="../index.php">戻る</a>
-                        </span>
-                    </form>
-                </div>
+<!-- <?php include('layouts/header.php'); ?>-->
+ <link href="js/material-kit.css?v=2.0.5" rel="stylesheet" />
+<body style="margin-top: 60px">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2 thumbnail">
+                <h2 class="text-center content_header">サインイン</h2>
+                <form method="POST" action="login.php" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="email">メールアドレス</label>
+                        <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+                        <?php if(isset($errors['signin']) && $errors['signin'] =='blank'): ?>
+                            <p class= 'text-danger'>メールアドレスとパスワードを正しく入力してください</p>
+                        <?php endif; ?>
+                        <?php if(isset($errors['signin']) && $errors['signin'] =='failed'): ?>
+                            <p class= 'text-danger'>サインインに失敗しました</p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">パスワード</label>
+                        <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+                    </div>
+                    <input type="submit" class="btn btn-info" value="サインイン">
+                    <span style="float: right; padding-top: 6px;">
+                        <a href="../index.php">戻る</a>
+                    </span>
+                </form>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+<!-- <?php include('layouts/header.php'); ?> -->
 </html>
