96eec2e2c45077d1c536dacfc21ec1acddb7f71b
yasuyukitakeshita <thetake1206@gmail.com>
2019-03-07 23:50:24 +0800

    途中です。

--- admin/edit.php
+++ admin/edit.php
@@ -62,8 +62,18 @@ if(!empty($_POST)){
 
     // $q_qs[] = $q_q;
     }
-
-header('Location: edit.php');
+    if($o = 4; $o < 49; $o++){
+    $o_sql = '
+    UPDATE `options`
+    SET `content` = ?
+        `price` = ?
+    WHERE `question_id` = ?
+    ';
+    $o_data = [$_POST['o'.$o],$_POST['o_pr_'.$o],$o];
+    $o_stmt = $dbh->prepare($o_sql);
+    $o_stmt->execute($o_data);
+    }
+    header('Location: edit.php');
 }
 
 
@@ -86,68 +96,268 @@ header('Location: edit.php');
 <form method="POST" action="edit.php">
 
 <h1>問題2</h1>
-<!-- textarea内にユーザーが記述したものはそのままvalueになる -->
+<p>問題</p>
 <textarea name = "q2"><?php echo $questions[1]['content']; ?></textarea><br>
-<!-- <p>問題2の回答1</p>
-<!-- <textarea name = "q2"><?php echo $questions[1]['content']; ?></textarea><br> -->
- -->
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[4]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[4]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[5]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[5]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[6]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[6]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題3</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q3"><?php echo $questions[2]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[7]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[7]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[8]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[8]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[9]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[9]['price']; ?></textarea><br>
+<br><br><br>
+
+
 
 <h1>問題4</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q4"><?php echo $questions[3]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[10]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[10]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[11]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[11]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[12]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[12]['price']; ?></textarea><br>
+<br><br><br>
+
 
 <h1>問題5</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q5"><?php echo $questions[4]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[13]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[13]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[14]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[14]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[15]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[15]['price']; ?></textarea><br>
+<br><br><br>
+
+
 
 <h1>問題6</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q6"><?php echo $questions[5]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[16]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[16]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[17]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[17]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[18]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[18]['price']; ?></textarea><br>
+<br><br><br>
+
 
 <h1>問題7</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q7"><?php echo $questions[6]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[19]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[19]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[20]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[20]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[21]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[21]['price']; ?></textarea><br>
+<br><br><br>
+
 
 <h1>問題8</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q8"><?php echo $questions[7]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[22]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[22]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[23]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[23]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[24]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[24]['price']; ?></textarea><br>
+<br><br><br>
+
 
 <h1>問題9</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q9"><?php echo $questions[8]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[25]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[25]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[26]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[26]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[27]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[27]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題10</h1>
 <p>問題文</p>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q10"><?php echo $questions[9]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[28]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[28]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[29]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[29]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[30]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[30]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題11</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q11"><?php echo $questions[10]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[31]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[31]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[32]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[32]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[33]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[33]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題12</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q12"><?php echo $questions[11]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[34]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[34]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[35]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[35]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[36]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[36]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題13</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q13"><?php echo $questions[12]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[37]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[37]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[38]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[38]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[39]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[39]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題14</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q14"><?php echo $questions[13]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[40]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[40]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[41]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[41]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[42]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[42]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題15</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q15"><?php echo $questions[14]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[43]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[43]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[44]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[44]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[45]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[45]['price']; ?></textarea><br>
+<br><br><br>
 
 <h1>問題16</h1>
 <!-- textarea内にユーザーが記述したものはそのままvalueになる -->
 <textarea name = "q16"><?php echo $questions[15]['content']; ?></textarea><br>
+<p>回答1</p>
+<textarea name = "o4"><?php echo $options[46]['content']; ?></textarea><br>
+<p>回答1の値段</p>
+<textarea type = 'number' name = "o_pr_4"><?php echo $options[46]['price']; ?></textarea><br>
+<p>回答2</p>
+<textarea name = "o5"><?php echo $options[47]['content']; ?></textarea><br>
+<p>回答2の値段</p>
+<textarea type = 'number' name = "o_pr_5"><?php echo $options[47]['price']; ?></textarea><br>
+<p>回答3</p>
+<textarea name = "o6"><?php echo $options[48]['content']; ?></textarea><br>
+<p>回答3の値段</p>
+<textarea type = 'number' name = "o_pr_6"><?php echo $options[48]['price']; ?></textarea><br>
+<br><br><br>
 
 <input type = "submit" class="btn btn-primary" value="変更">
 
