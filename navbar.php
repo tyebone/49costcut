f461344aa9a91f4324eaf01dc567a28defab1473
内田佳世 <pauole.noah.hoku@gmail.com>
2019-03-12 21:02:08 +0800

    navber修正

--- navbar.php
+++ navbar.php
@@ -6,17 +6,26 @@
             メニュー
           </button>
           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="z-index:9999">
-            <?php if (!isset($_SESSION['49_CostCut'])):?>
-            <a class="dropdown-item" href="../index.php">TOP </a>
-            <a class="dropdown-item" href="プロフィール/profile.html">About</a>
-            <a class="dropdown-item" href="signout.php">ログアウト</a>
+            <?php if (isset($_SESSION['49_CostCut'])):?>
+            <a class="dropdown-item" href="count.php">管理者画面TOP</a>
             <a class="dropdown-item" href="edit.php">編集</a>
+            <a class="dropdown-item" href="signout.php">ログアウト</a>
             <?php else :?>
             <a class="dropdown-item" href="index.php">TOP </a>
-            <a class="dropdown-item" href="admin/login.php">管理者ログイン</a>
             <a class="dropdown-item" href="プロフィール/profile.html">About</a>
+            <a class="dropdown-item" href="admin/login.php">管理者ログイン</a>
             <?php endif;?>
           </div>
         </div>
       </nav>
     </header>
+   <!--  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="z-index:9999">
+            <a class="dropdown-item" href="index.php">TOP </a>
+            <a class="dropdown-item" href="プロフィール/profile.html">About</a>
+            <?php if (!isset($_SESSION['49_CostCut'])):?>
+            <a class="dropdown-item" href="admin/login.php">管理者ログイン</a>
+            <?php else :?>
+             <a class="dropdown-item" href="edit.php">編集</a>
+            <a class="dropdown-item" href="signout.php">ログアウト</a>
+            <?php endif;?>
+          </div> -->
